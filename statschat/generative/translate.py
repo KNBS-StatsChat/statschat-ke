938fd85a719c446f169bccae893d8e2b75148acc
josh-ons2022 <118263780+josh-ons2022@users.noreply.github.com>
2025-03-25 16:58:32 +0000

    updated translate.py

--- statschat/generative/translate.py
+++ statschat/generative/translate.py
@@ -1,40 +1,21 @@
-from transformers import pipeline
+# %%
+from transformers import pipeline, AutoTokenizer
 from dotenv import load_dotenv
 import os
 
-#pip install sacremoses
-#pip install sentencepiece
-
+# %%
 # Load the token for Hugging Face
 load_dotenv()
 sec_key = os.getenv("HF_TOKEN")
 
-text = "The weather outside is good"
+#english_text = "Where can I find the registered births by age of mother and county?"
+english_text = "How is core inflation calculated?"
 
-#translate = pipeline("translation", model="Rogendo/en-sw")
-
-#print(translate(text))
-
-#def translate_question(text):
-    
- #   translation = pipeline("translation", model="Rogendo/en-sw")
-    
-  #  answer = translation(text)
-    
-   # print(answer)
-    #print(list[(answer)])
-    
-    #return answer
-
-#translate_question(text)
-
-def translate_question(text):
+def english_to_swahili(english_text):
     
     translation = pipeline("translation", model="Rogendo/en-sw")
         
-    translated_text = translation(text)
-        
-    #print(answer)
+    translated_text = translation(english_text)
 
     for dictionary in translated_text:
         for translation in dictionary.values():
@@ -42,7 +23,35 @@ def translate_question(text):
             
     return translation
 
-translate_question(text)
 
+english_to_swahili(english_text)
 
+# %%
+swahili_text = "Ninaweza kupata wapi watoto walioandikishwa wakati wa umri wa mama na jimbo?"
 
+def swahili_to_english(swahili_text):
+    
+    translation = pipeline("translation", 
+                           model="Rogendo/sw-en",  
+                           tokenizer = AutoTokenizer.from_pretrained("Rogendo/sw-en"))
+    
+    #prompt = """You are an AI assistant that translates text from Swahili to English. 
+    # The text will be a question from the public on official statistics from the Kenya National Bureau of Statistics. 
+    # Ensure the translated sentence makes sense grammatically in english.
+    
+    
+    #"""
+        
+    translated_text = translation(swahili_text)
+        
+    for dictionary in translated_text:
+        for translation in dictionary.values():
+            print(translation)
+            
+    return translation
+
+swahili_to_english(swahili_text)
+
+# seems like prompts can't be added to this to finetune
+
+# https://dataloop.ai/library/model/rogendo_sw-en/#capabilities
