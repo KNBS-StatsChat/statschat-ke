92ddec20da666fea2c7d3521a2a02e3046d4e95f
josh-ons2022 <118263780+josh-ons2022@users.noreply.github.com>
2025-03-26 11:43:57 +0000

    nllb model for swahili to english

--- statschat/generative/translate.py
+++ statschat/generative/translate.py
@@ -63,10 +63,10 @@ model_name = "facebook/nllb-200-distilled-600M"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
-tokenizer.src_lang = "rus_Cyrl"
-inputs = tokenizer(text="поля озарились утренним солнцем", return_tensors="pt")
+tokenizer.src_lang = "swh_Latn"
+inputs = tokenizer(text="Tembo", return_tensors="pt")
 translated_tokens = model.generate(
     **inputs, forced_bos_token_id=tokenizer.convert_tokens_to_ids("eng_Latn")
 )
 print(tokenizer.decode(translated_tokens[0], skip_special_tokens=True))
-# %%
+
